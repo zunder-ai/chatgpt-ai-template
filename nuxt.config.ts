c6b9e56a4c4de7e574a278d481e20f212cb54b4c
regenrek <hello@regenrek.at>
2024-08-24 09:45:37 +0200

    update code examples

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+
+  extends: ['@zunderai/ui'],
+  modules: ['@nuxt/ui'],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  runtimeConfig: {
+    openaiApiKey: process.env.OPENAI_API_KEY,
+    openaiModel: process.env.OPENAI_MODEL,
+    public: {
+      useSimulatedChat: process.env.USE_SIMULATED_CHAT === "true",
+    },
+  },
+
+  experimental: {
+    componentIslands: true,
+  },
 })
